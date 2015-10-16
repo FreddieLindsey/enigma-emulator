b574e86e5e799301de6140664ed6d0e44dd6038e
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-10-16 12:20:49 +0100

    Added PlugBoard to EnigmaMachine

--- EnigmaMachine.cpp
+++ EnigmaMachine.cpp
@@ -2,6 +2,7 @@
 
 void EnigmaMachine::receive(const char& c) {
   char character_encode(c);
+  pb.map(character_encode);
   refl.reflect(character_encode);
   std::cout << character_encode;
 }
--- EnigmaMachine.hpp
+++ EnigmaMachine.hpp
@@ -2,10 +2,12 @@
 #define EnigmaMachine_h
 
   #include <iostream>
+  #include "PlugBoard.hpp"
   #include "Reflector.hpp"
 
   class EnigmaMachine {
     private:
+      PlugBoard pb;
       Reflector refl;
     public:
       void receive(const char& c);
