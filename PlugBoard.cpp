09ea8b8256f45d3d6c082b0a53d02048d1f04eb4
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-10-16 11:44:56 +0100

    Changed arguments for PlugBoard initialisation

--- PlugBoard.cpp
+++ PlugBoard.cpp
@@ -1,1 +1,15 @@
 #include "PlugBoard.hpp"
+
+using namespace std;
+
+PlugBoard::PlugBoard(string file_content) {
+
+}
+
+void PlugBoard::encode(char& c) {
+
+}
+
+void PlugBoard::decode(char& c) {
+
+}
--- PlugBoard.hpp
+++ PlugBoard.hpp
@@ -1,10 +1,19 @@
 #ifndef PlugBoard_h
 #define PlugBoard_h
 
+  #include <iostream>
+  #include <sstream>
+  #include <fstream>
+  #include <tuple>
+  #include <vector>
+
+  using namespace std;
+
   class PlugBoard {
     private:
-      int map_[26];
+      vector<tuple<int, char>> map_;
     public:
+      PlugBoard(string file_content);
       void encode(char& c);
       void decode(char& c);
   };
