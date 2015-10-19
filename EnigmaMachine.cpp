6d3df45a2fe9b6db497003762459d36ece44f348
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-10-19 16:34:14 +0100

    Added initialisation of rotors in EnigmaMachine

--- EnigmaMachine.cpp
+++ EnigmaMachine.cpp
@@ -1,7 +1,11 @@
 #include "EnigmaMachine.hpp"
 
 EnigmaMachine::EnigmaMachine(int ALPHABET_SIZE, string& plugboard, vector<string>& rotors)
-  : pb(plugboard, ALPHABET_SIZE) {}
+  : pb(plugboard, ALPHABET_SIZE), rts(rotors.size()) {
+    for (size_t i = 0; i < rts.size(); i++) {
+      rts[i] = new Rotor(rotors[i]);
+    }
+}
 
 void EnigmaMachine::receive(char& c) {
   // FORWARDS DIRECTION OF TRAVEL
