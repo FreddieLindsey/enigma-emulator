3d8bf3a8a70794e584cf6e5012a1d92e612e9cc4
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-10-27 09:16:09 +0000

    Verbose

--- big_test.sh
+++ big_test.sh
@@ -28,7 +28,9 @@ if [[ $OS == 'Darwin' ]]; then
   BOB=gdate
 fi
 
+echo -e "Starting process...\n"
 T=$($BOB +%s%6N)
 eval $COMMAND
 T_=$($BOB +%s%6N)
+echo -e "Finished process...\n"
 echo -e "Time taken:\t0.$((T_ - T)) seconds"
